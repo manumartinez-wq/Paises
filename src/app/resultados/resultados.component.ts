f1e54ec72939eca039f7f741a0a4947c9bc6623d
Sebaxtian Gonzalez <manumartinez-wq>
2021-07-16 20:48:09 -0500

    taller pagina paises

---
+++ src/app/resultados/resultados.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { TaskService } from '../services/task.service';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styles: [
+  ]
+})
+export class ResultadosComponent {
+
+  get resultados(){
+    return [this.tasksService.resultados];
+  }
+
+  constructor( private tasksService: TaskService) { }
+
+}
